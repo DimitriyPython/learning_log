2d3d405eeebdafba6fd12fbdca0e3187c844c3a3
DimitriyPython <dimitrypython@gmail.com>
2022-07-20 11:53:07 +0300

    Initial commit

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,13 @@
 """ Defines schemas URL for  users """
 
 from django.urls import path, include
+from . import views
 
 app_name = 'users'
 urlpatterns = [
     # On default URL authorization
     path('', include('django.contrib.auth.urls')),
+    # Page of registration
+    path('register/', views.register, name='register'),
 ]
 
--- users/views.py
+++ users/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 
-# Create your views here.
+
+def register(request):
+    """Registring a new user"""
+    if request.method != 'POST':
+        # Enter an empty form of registration
+        form = UserCreationForm()
+    else:
+        # Processing of full form
+        form = UserCreationForm(data=request.POST)
+
+        if form.is_valid():
+            new_user = form.save()
+            # Log in and redirect on homepage
+            login(request, new_user)
+            return request('learning_logs:index')
+
+    # Output empty or invalid form
+    context = {'form': form}
+    return render(request, 'users/register.html', context)
